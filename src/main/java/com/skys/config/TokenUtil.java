0745ccc7fe02c8562c070c070e4f0f62b9f03127
WindHide <WindHide520@gmail.com>
2022-07-10 22:16:56 +0800

    [A]添加了在线人数查询的新功能，但是没有写controller而且前端并没有使用

--- src/main/java/com/skys/config/TokenUtil.java
+++ src/main/java/com/skys/config/TokenUtil.java
@@ -7,10 +7,16 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.skys.pojo.Account;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TokenUtil {
     private static final long EXPIRE_TIME= 10*60*30*1000;
+
+    private static final long ONLINE_TIME = 60*1000;
     private static final String TOKEN_SECRET="Sky_WindHide";  //密钥盐
+
+    private static Map<String,String> onlineCountMap = new HashMap();
     /**
      * 签名生成
      * @param account
@@ -26,6 +32,8 @@ public class TokenUtil {
                     .withExpiresAt(expiresAt)
                     // 使用了HMAC256加密算法。
                     .sign(Algorithm.HMAC256(TOKEN_SECRET));
+            onlineCountMap.put(account.getAccountUsername(),token + "-Time:"+new Date().getTime());
+            // 这里放一个OnlineCountMap put数据用来统计一下在线的人数。
         } catch (Exception e){
             e.printStackTrace();
         }
@@ -44,9 +52,40 @@ public class TokenUtil {
             System.out.println("认证经过：");
             System.out.println("username: " + jwt.getClaim("username").asString());
             System.out.println("过时时间：      " + jwt.getExpiresAt());
+            String username = jwt.getClaim("username").asString();
+            if(onlineCountMap.containsKey(username)){
+                for(String keys : onlineCountMap.keySet()){
+                    if(keys.equals(username)){
+                        Long oldTime = Long.parseLong(onlineCountMap.get(username).split("-Time:")[1]);
+                            onlineCountMap.put(keys,token + "-Time:"+new Date().getTime());
+                            System.out.println("时间重置==============重置前剩余"+(new Date().getTime() - oldTime)/1000+"秒");
+                            System.out.println("当前在线人数"+getOnlineCount());
+                        break;
+                    }
+                }
+            }else{
+                onlineCountMap.put(username,token + "-Time:"+new Date().getTime());
+                System.out.println("当前在线人数"+getOnlineCount());
+            }
             return true;
         } catch (Exception e){
+            e.printStackTrace();
             return false;
         }
+    }
+
+    /**
+     *
+     * 在线人数统计
+     * 60000 == 60s
+     */
+    public static Integer getOnlineCount(){
+        for(String keys : onlineCountMap.keySet()){
+            Long oldTime = Long.parseLong(onlineCountMap.get(keys).split("-Time:")[1]);
+            if((new Date().getTime() - oldTime) >= ONLINE_TIME){
+                onlineCountMap.remove(keys);
+            }
+        }
+        return onlineCountMap.size();
     }
 }
